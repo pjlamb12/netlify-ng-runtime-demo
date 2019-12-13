4f7db40cb5e6cde73e3523d062527ab98b02648d
Preston Lamb <preston.j.lamb@gmail.com>
2019-12-13 07:50:59 -0700

    pull all runtime_ prefixed variables from process.env

--- .netlify/functions/get-runtime-config-data.js
+++ .netlify/functions/get-runtime-config-data.js
@@ -1,10 +1,24 @@
+function buildConfigObject() {
+  const config = {};
+  for (let attr in process.env) {
+    if (attr.startsWith("runtime_")) {
+      const startAt = attr.indexOf("_") + 1;
+      const newKey = attr.substring(startAt);
+      config[newKey] = process.env[attr];
+    }
+  }
+
+  return config;
+}
+
 exports.handler = function(event, context, callback) {
-  const { attribute } = process.env;
-
-  console.log("function hit, returning: ", { attribute });
-
-  return callback(null, {
-    body: JSON.stringify({ attribute }),
-    statusCode: 200
-  });
+  try {
+    const config = buildConfigObject();
+    const returnObj = { body: JSON.stringify(config), statusCode: 200 };
+    console.log("runtime config data: ", JSON.stringify(returnObj, null, 2));
+    return callback(null, returnObj);
+  } catch (error) {
+    console.error(error);
+    return callback({ statusCode: 500, error });
+  }
 };
